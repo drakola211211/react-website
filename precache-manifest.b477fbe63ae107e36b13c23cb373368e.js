3e7707037fa0fd2ce8b993423652141c3f4c7986
drakola211211 <sekakoko98765@gmail.com>
2020-10-12 19:52:09 +0200

    Updates

---
+++ precache-manifest.b477fbe63ae107e36b13c23cb373368e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822242bbf41c6dd8ee81aa8ceb44d0af",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "0a8ceb7c9492253126e5",
+    "url": "/react-website/static/css/main.bf38e796.chunk.css"
+  },
+  {
+    "revision": "874e7bef0f07a75639ec",
+    "url": "/react-website/static/js/2.638c8322.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-website/static/js/2.638c8322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8ceb7c9492253126e5",
+    "url": "/react-website/static/js/main.e83096f4.chunk.js"
+  },
+  {
+    "revision": "fadfad2d28377605b9ac",
+    "url": "/react-website/static/js/runtime-main.caee497b.js"
+  }
+]);
